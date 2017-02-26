03bb18c54eb6768284aefb9675a5a91b899b63cc
Zak-Kent <zak.kent@gmail.com>
2017-02-26 14:42:24 -0800

    Update models.py

--- example_project/example_app/models.py
+++ example_project/example_app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Songs(models.Model):
-    id = models.AutoField(primary_key=True)
     artist_name = models.CharField(max_length=255, default='')
     song_title = models.CharField(max_length=255, default='')
     song_year = models.IntegerField(blank=True, null=True)
